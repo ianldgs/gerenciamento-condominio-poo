123ce9328287a8a190f602a1f4e0fb889ca8395d
Matheus Elias R. Moreira <mattelias@live.com>
2016-11-01 19:47:05 -0200

    commit inicial

---
+++ src/main/java/com/cotemig/Application.java
@@ -0,0 +1,17 @@
+package com.cotemig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Created by matheus.elias on 10/31/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/cotemig/GreetingController.java
@@ -0,0 +1,21 @@
+package com.cotemig;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+/**
+ * Created by matheus.elias on 10/31/16.
+ */
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
