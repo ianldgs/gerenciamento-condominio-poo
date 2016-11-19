2f4f86d2af6b25495e2e2049c2d3e828a403c0dd
Matheus Elias R. Moreira <mattelias@live.com>
2016-11-19 16:37:11 -0200

    Added spring-data-jpa to pom.xml and added an extend to condorepository

--- src/main/java/com/cotemig/repositories/CondoRepository.java
+++ src/main/java/com/cotemig/repositories/CondoRepository.java
@@ -1,13 +1,14 @@
 package com.cotemig.repositories;
 
 import com.cotemig.models.Condo;
+import org.springframework.data.repository.Repository;
 
 import java.util.List;
 
 /**
  * Created by matheus.elias on 11/2/16.
  */
-public interface CondoRepository {
+public interface CondoRepository extends Repository<Condo, Long> {
     public void addCondo(Condo condo);
     public void updateCondo(Condo condo);
     public List<Condo> listCondos();
