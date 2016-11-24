8dffd0df01766d3d8404f1db9b1b84b55126f31f
Ian Luca <ian.naph@gmail.com>
2016-11-24 01:41:03 -0200

    Colocando anotação @ComponentScan na config de testes

--- src/tests/java/com/cotemig/TestsConfig.java
+++ src/tests/java/com/cotemig/TestsConfig.java
@@ -2,6 +2,7 @@ package com.cotemig;
 
 import com.cotemig.interceptors.AuthInterceptor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -23,6 +24,7 @@ import java.sql.SQLException;
  */
 @Configuration
 @EnableJpaRepositories
+@ComponentScan
 @EnableTransactionManagement
 public class TestsConfig extends WebMvcConfigurerAdapter {
     @Bean
